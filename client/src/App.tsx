7d5040d8e7d9babbaf51c8f9a73ecdce9e353ba8
JiYJ725 <wldudwns07@naver.com>
2022-08-24 22:46:39 +0900

    do proxy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,16 +11,14 @@ function App() {
   const [hello, setData] = useState();
 
   useEffect(() => {
-    fetch(
-      'http://ec2-54-180-106-244.ap-northeast-2.compute.amazonaws.com:8080/hello'
-    )
+    fetch('/hello')
       .then((res) => res.json())
       .then((data) => {
         console.log(data.hello[0].text);
         setData(data.hello[0].text);
       });
   }, []);
-  return <div className="App">{hello}</div>;
+  return <div className="App">==============team30</div>;
 }
 
 export default App;
